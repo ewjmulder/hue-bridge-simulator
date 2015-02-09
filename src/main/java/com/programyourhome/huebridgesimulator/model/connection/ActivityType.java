0bbd88bd46fb2a86a8c20848a23a88a674b45e9d
Erik Mulder <erik.mulder@qsd.nl>
2015-02-10 00:09:36 +0100

    refinement of type enum

--- src/main/java/com/programyourhome/huebridgesimulator/model/connection/ActivityType.java
+++ src/main/java/com/programyourhome/huebridgesimulator/model/connection/ActivityType.java
@@ -2,9 +2,10 @@ package com.programyourhome.huebridgesimulator.model.connection;
 
 public enum ActivityType {
 
-    CONNECT, DISCONNECT,
+    CONNECT,
+    DISCONNECT,
     GET_DESCRIPTION,
-    GET_LIGHTS, GET_LIGHT,
-    PUT_LIGHT;
+    GET_LIGHTS,
+    SET_LIGHT;
 
 }
